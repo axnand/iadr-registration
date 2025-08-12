157676b9f82eb16e4f3e57b49fb86850ff5292fb
Anand <anandmohanjha241@gmail.com>
2025-08-12 17:13:38 +0530

    made the section responsive

--- src/app/pcc-registration/PCCCard.jsx
+++ src/app/pcc-registration/PCCCard.jsx
@@ -15,23 +15,23 @@ export default function PCCCard({ title, type, conductors, fee, code, imageUrl }
         </div>
       )}
       <div className="p-4 flex-1 flex flex-col items-start">
-         <span className={`px-2 py-1 rounded-full text-sm font-medium ${badgeColor}`}>{type}</span>
-        <h3 className="mt-2 text-lg font-bold text-gray-900">{title}</h3>
+         <span className={`px-2 py-1 rounded-full sm:text-sm text-xs font-medium ${badgeColor}`}>{type}</span>
+        <h3 className="mt-2 sm:text-lg text-md font-bold text-gray-900">{title}</h3>
         <div className="mt-3">
             <h1 className="text-sm pb-1 font-semibold text-gray-800">Conductors:</h1>
         {conductors.map((name, index) => (
-            <p key={index} className="text-sm text-gray-600 font-semibold">
+            <p key={index} className="sm:text-sm text-[13px] text-gray-600 font-semibold">
             {name}
             </p>
         ))}
         </div>
       </div>
       <div className="flex flex-col py-6  border-t-2 mx-4 mt-2">
-        <p className="text-xl  text-zinc-900 mt-4 font-bold">Fee: ₹{fee}</p>
+        <p className="sm:text-xl text-lg  text-zinc-900 mt-4 font-bold">Fee: ₹{fee}</p>
         <p className="text-sm mt-1 font-medium text-gray-500">Code: {code}</p>
       <button
         onClick={() => alert(`Registering for ${code}`)}
-        className="mt-4  bg-indigo-600 text-white py-2 font-semibold  rounded-lg hover:bg-indigo-700 transition"
+        className="mt-4 sm:text-base text-[15px] bg-indigo-600 text-white py-2 font-semibold  rounded-lg hover:bg-indigo-700 transition"
       >
         Register
       </button>
